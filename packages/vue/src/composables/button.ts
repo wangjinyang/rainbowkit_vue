8337335a96e2af7895a5481b293524feb17ac5ee
Ho Siang Toon <hosiangtoon@gmail.com>
2024-06-11 01:16:17 +0800

    Fixed release workflow v3

--- packages/vue/src/composables/button.ts
+++ packages/vue/src/composables/button.ts
@@ -1,12 +1,12 @@
 import { WalletButtonContextKey, type Context, type WalletButtonContext } from '@/types'
-import { inject, ref, toRefs,reactive } from 'vue'
+import { inject, ref, toRefs,reactive, ToRefs } from 'vue'
 
 export function createWalletButtonContext(): Context<WalletButtonContext>{
     const context = ref<WalletButtonContext>({ connector: null });
     return context;
 }
 
-export function useWalletButtonContext() {
+export function useWalletButtonContext(): ToRefs<WalletButtonContext> {
     const context = inject(WalletButtonContextKey);
     if (!context) throw Error(`Could not find injected '${String(WalletButtonContextKey)}' instance.`)
     return toRefs(reactive(context.value));
